dc7ea1dce70e8de660999263e93e4cbd46779982
julienpillis <pllsjulien@gmail.com>
2023-09-25 20:29:13 +0200

    implemented ApprovalSWF & ApprovalSCF

--- ia04/comsoc/approval.go
+++ ia04/comsoc/approval.go
@@ -1,9 +1,29 @@
 package comsoc
 
-// test comment
 func ApprovalSWF(p Profile, thresholds []int) (count Count, err error) {
-	return nil, nil
+	err = CheckProfile(p) // à voir quelle(s) vérifications on doit faire
+	if err != nil {
+		return nil, err
+	}
+	count = make(map[Alternative]int)
+	for index_profile, pref := range p {
+		for _, key := range pref[:thresholds[index_profile]] {
+			// On itère uniquement entre l'indice 0 et le seuil associé (indice exclu)
+			_, exist := count[key]
+			if exist {
+				count[key]++
+			} else {
+				count[key] = 1
+			}
+		}
+	}
+	return count, nil
 }
 func ApprovalSCF(p Profile, thresholds []int) (bestAlts []Alternative, err error) {
-	return nil, nil
+	var count Count
+	count, err = ApprovalSWF(p, thresholds)
+	if err != nil {
+		return nil, err
+	}
+	return maxCount(count), err
 }
