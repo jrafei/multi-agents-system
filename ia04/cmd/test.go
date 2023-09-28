3d60248a57a42ca32edaba81ded89ab9cc3059e1
julienpillis <pllsjulien@gmail.com>
2023-09-28 20:03:48 +0200

    began to implement STV

--- ia04/cmd/test.go
+++ ia04/cmd/test.go
@@ -1,6 +1,9 @@
 package main
 
-
+import (
+	"ia04/comsoc"
+	"fmt"
+)
 
 func main() {
 
@@ -79,23 +82,35 @@ func main() {
 		fmt.Println(res)
 	*/
 	/*
-	prefs := [][]comsoc.Alternative{
+		prefs := [][]comsoc.Alternative{
+			{1, 3, 2},
+			{1, 2, 3},
+			{2, 3, 1},
+			{4, 1, 2},
+		}
+		thresholds := []int{2, 1, 2, 3}
+
+		res, err := comsoc.ApprovalSCF(prefs, thresholds)
+
+		if err != nil {
+			fmt.Println(err)
+		}
+		if len(res) != 1 || res[0] != 1 {
+			fmt.Println("error, 1 should be the only best Alternative")
+		}
+
+		fmt.Println(res)
+	*/
+
+	p := [][]comsoc.Alternative{
 		{1, 3, 2},
 		{1, 2, 3},
 		{2, 3, 1},
-		{4, 1, 2},
+		{3, 1, 2},
 	}
-	thresholds := []int{2, 1, 2, 3}
 
-	res, err := comsoc.ApprovalSCF(prefs, thresholds)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-	if len(res) != 1 || res[0] != 1 {
-		fmt.Println("error, 1 should be the only best Alternative")
-	}
+	res, _ := comsoc.STV_SWF(p)
 
 	fmt.Println(res)
-	*/
+
 }
